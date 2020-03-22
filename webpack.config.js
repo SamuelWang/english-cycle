89d2f1ed19fa64ef26dc91b9553e1fb50def3e65
Samuel Wang <chieh0919@gmail.com>
2020-03-22 23:33:49 +0800

    Set up production configurations. Install Bootstrap and BootstrapVue. Add favicon settings.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,71 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue/,
-        loader: 'vue-loader'
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin(),
-    new VueLoaderPlugin()
-  ]
+module.exports = (env, options) => {
+  const devMode = options.mode !== 'production';
+
+  let configurations = {
+    mode: devMode ? 'development' : 'production',
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+      contentBase: './dist',
+      hot: true,
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'English Cycle',
+      }),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      }),
+      new FaviconsWebpackPlugin('./src/images/favicon.png'),
+      new VueLoaderPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/i,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                esModule: true,
+                hmr: devMode,
+              },
+            },
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.vue/,
+          loader: 'vue-loader',
+        },
+      ],
+    },
+  };
+
+  if (devMode) {
+    configurations.devtool = 'source-map';
+  } else {
+    configurations.optimization = {
+      splitChunks: {
+        chunks: 'all'
+      },
+      minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    };
+  }
+
+  return configurations;
 };
