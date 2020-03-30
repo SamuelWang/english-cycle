501f8f85cae0ae8db5254ad79d929a985c25b106
Samuel Wang <chieh0919@gmail.com>
2020-03-30 22:48:15 +0800

    Set up the configurations of Firebase.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import App from './components/App.vue';
 import router from './router';
 import store from './store';
+import appSettings from '../app-settings'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Import firebase and its modules
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
+initFirebase();
 initAppNode();
 
 const app = new Vue({
@@ -26,4 +33,16 @@ function initAppNode() {
   appNode.id = 'app';
   appNode.innerHTML = '<app><app>';
   document.body.appendChild(appNode);
+}
+
+function initFirebase() {
+  const firebaseConfig = {
+    apiKey: appSettings.firebase.apiKey,
+    authDomain: appSettings.firebase.authDomain,
+    databaseURL: appSettings.firebase.databaseURL,
+    projectId: appSettings.firebase.projectId,
+    appId: appSettings.firebase.appId
+  };
+
+  firebase.initializeApp(firebaseConfig);
 }
