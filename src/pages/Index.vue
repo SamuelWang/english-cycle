8d9d36e97a7b7f242ac0c9641cbd32b4236c1f14
Samuel Wang <chieh0919@gmail.com>
2020-05-23 21:03:59 +0800

    Set the button link of the jumbotron of the index page.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,18 +5,31 @@
       header="English Cycle"
       lead="This App helps you to learn English well by reviewing again and again."
     >
-      <p>As forgetting curve theory said if you review immediately after learned a new item, it will be hard to forget the item you learned.</p>
-      <b-button variant="primary" href="#">Start Learning</b-button>
+      <p>
+        As forgetting curve theory said if you review immediately after learned
+        a new item, it will be hard to forget the item you learned.
+      </p>
+      <b-button variant="primary" href="#" :to="{ path: '/login' }"
+        >Start Learning</b-button
+      >
     </b-jumbotron>
     <div class="container-md py-5">
       <div class="row">
         <div class="col-md-6">
           <h2>Backlog</h2>
-          <p>When you get a new item, like vocabulary, phrase, sentence and so on, you can put it into the backlog first, and learn it when you have a spare moment.</p>
+          <p>
+            When you get a new item, like vocabulary, phrase, sentence and so
+            on, you can put it into the backlog first, and learn it when you
+            have a spare moment.
+          </p>
         </div>
         <div class="col-md-6">
           <h2>Cycle</h2>
-          <p>After you learned a new item and took note, in the cycle list, the item will be sorted by the number of times of reviewing from less to more. Then you can review the item you are not familiar with.</p>
+          <p>
+            After you learned a new item and took note, in the cycle list, the
+            item will be sorted by the number of times of reviewing from less to
+            more. Then you can review the item you are not familiar with.
+          </p>
         </div>
       </div>
     </div>
